741aacf4aeb49db5f6f99678894aa4031b0f5a98
Ravi Kapadia <139764414+rvikapadia9@users.noreply.github.com>
2025-09-03 07:30:38 +0530

    Add FirstJavaProgram and SecondProgram

---
+++ FirstJavaProgram.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class FirstJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to java");
+       int Num=10;
+        int Number =11;
+        System.out.println(Num+Number);
+    }
+}
