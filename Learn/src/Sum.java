9b150808238d2205b95e8f55fc4e96ad2618f88f
Ravi Kapadia <139764414+rvikapadia9@users.noreply.github.com>
2025-09-12 18:04:47 +0530

    Sum By Intellij

---
+++ Learn/src/Sum.java
@@ -0,0 +1,9 @@
+package Learn;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+        System.out.println("Sum of a and b is: " + (a + b));
+    }
+}
